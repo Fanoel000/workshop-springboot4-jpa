11813ed0896fa441ef2de6d8947f9ac90b495b15
Fanoel_Carvalho <eletrotech5@gmail.com>
2026-04-22 03:41:59 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.course.entities.Category;
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import com.educandoweb.course.entities.Product;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.educandoweb.course.repositories.CategoryRepository;
+import com.educandoweb.course.repositories.OrderItemRepository;
 import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.ProductRepository;
 import com.educandoweb.course.repositories.UserRepository;
@@ -21,73 +23,86 @@ import com.educandoweb.course.repositories.UserRepository;
 //Criado a classe especifica para testes
 @Configuration
 @Profile("test")
-public class TestConfig implements CommandLineRunner { //Ver interface CommandLineRunner
-	//userRepository é um atributo do tipo classe UserRepository
+public class TestConfig implements CommandLineRunner { // Ver interface CommandLineRunner
+	// userRepository é um atributo do tipo classe UserRepository
 	@Autowired
 	private UserRepository userRepository; //
-	
-	//Salvar alguns pedidos na carga inicial do banco de cados => fazer o CID
+
+	// Salvar alguns pedidos na carga inicial do banco de cados => fazer o CID
 	@Autowired
 	private OrderRepository orderRepository;
-	
-	//Criar um mecanismo de injeção de dependencia 
-	//Implícito automatico
-	//para um objeto depender de outro
-	//para associar uma instância do userRepository no caso os u1 e u2
-	//no TestConfig usa o @Autowired
-	
+
+	// Criar um mecanismo de injeção de dependencia
+	// Implícito automatico
+	// para um objeto depender de outro
+	// para associar uma instância do userRepository no caso os u1 e u2
+	// no TestConfig usa o @Autowired
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository; 
+
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
-		
-		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); 
-		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, ""); 
-		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, ""); 
-		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, ""); 
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-		
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		//Associa p1 categoria {The Lord of etc..} a cat2 {Books}
-		p1.getCategories().add(cat2);  
+
+		// Associa p1 categoria {The Lord of etc..} a cat2 {Books}
+		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
-		//Salvando os produtos com as associações em repository
+
+		// Salvando os produtos com as associações em repository
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");	
-	
-		//Padrão horario UTC, e pode converter p horario da região
-		//Instant converte, u1 é associação entre objetos Ver esquema diagrama
-		//Cria o primeiro pedido do usuário 1
-		//Cria o segundo pedido do usuário 2
-		//Cria o terceiro pedido do usuário 1
+
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+
+		// Padrão horario UTC, e pode converter p horario da região
+		// Instant converte, u1 é associação entre objetos Ver esquema diagrama
+		// Cria o primeiro pedido do usuário 1
+		// Cria o segundo pedido do usuário 2
+		// Cria o terceiro pedido do usuário 1
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:53:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+		// salva os objetos, os usuarios u1 e u2. em um arrayList dentro Repository
+		// Arrays.asList => cria um arrayList e logo diz salve as instâncias u1,u2
+		userRepository.saveAll(Arrays.asList(u1, u2));
+
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		//salva os objetos, os usuarios u1 e u2. em um arrayList dentro Repository
-    	//Arrays.asList => cria um arrayList e logo diz salve  as instâncias u1,u2
-				userRepository.saveAll(Arrays.asList(u1, u2));
-				
-				orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		//oi1 é um item de depido q é do pedido 1 product 1 quantidade 2
+		//O preço é a reprodução do p1
+		//Essa relação está no UML
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
+		
+		//Salvando no Banco no repository
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
-	
 
 }
--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 //Instant substitui o date Aula 278
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,6 +42,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	//Criar associação 1 p muitos na Order
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order () {
 		
@@ -86,6 +93,10 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Set<OrderItem> getItem(){
+		return items;
 	}
 
 	@Override
