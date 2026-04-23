29ded269133ca405a7dfc3d69e5e309515826210
Fanoel_Carvalho <eletrotech5@gmail.com>
2026-04-23 04:23:01 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -40,4 +40,18 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+	//Obs: Não se atualiza id e senhas.
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+		
+		
+	}
 }
